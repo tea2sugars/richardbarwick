a4d85d100ac0d9d8b635867f3b8f39672a438423
Rik Barwick <rik@creativitea.co.uk>
2018-04-09 12:10:37 +0100

    new page for HBC

---
+++ portfolio/hartestbrewingco.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Rik Barwick designs new brand and artwork for his new side-project - Hartest Brewing Co">
+	<meta name="author" content="">
+	
+	<title>Hartest Brewing Co | Branding and Label Design by Rik Barwick, Suffolk</title>
+	
+	<?php include('../assets/php/includes/head-css-internal.html');?>
+</head>
+<body>
+
+	<?php include('../assets/php/includes/nav-internal.html');?>
+
+
+	<!-- HOME -->
+	<section class="module module-header bg-dark bg-dark-70" data-background="../assets/images/projects/5mm_general_header.jpg">
+
+			<div class="container">
+
+				<!-- MODULE TITLE -->
+				<div class="row">
+
+					<div class="col-sm-6 col-sm-offset-3">
+
+						<h1 class="module-title align-center">5mm Interior Design Responsive Website design<br/><span class="small">Using the browser as a tool to deliver stunning imagery</span></h1>
+
+
+					</div>
+
+				</div>
+				<!-- /MODULE TITLE -->
+
+			</div>
+
+		</section >
+	<!-- /HOME -->
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		<!-- PORTFOLIO DESCRIPTION -->
+		<section class="module-small">
+
+			<div class="container">
+				
+				<!--<div class="row">
+					<div class="col-sm-12">
+
+						<p><img src="../assets/images/section-1.jpg" alt=""></p>
+					</div>
+				</div>-->
+				
+				<!-- PORTFOLIO KEY IMAGE -->
+				<div class="row">
+					<div class="col-sm-8 col-sm-offset-4">
+
+						<p><img src="../assets/images/projects/5mm_portfolio_03.jpg" alt="5mm Responsive Website Design"></p>
+					</div>
+				</div>
+
+				<!-- PORTFOLIO DESCRIPTION -->
+				<div class="row m-t-70">
+
+					<div class="col-md-4">
+						<div class="work-details">
+							<ul>
+								<li><strong>Client: </strong>5mm</li>
+								<li><strong>Project: </strong>Responsive Website Design & Development, Bootstrap, Perch CMS</li>
+								<li><strong>Online: </strong><a href="http://www.5mm.co" target="_blank">www.5mm.co</a></li>
+								<li><strong>Budget: </strong>££££</li>
+								<!--<li><strong>Testimonial: </strong>
+									<br/>
+									<blockquote>Being a new company, FiveThree had lots of things to worry about - the creation and development of a brand identity wasn't one of them thanks to the creative thinking at Creativitea.</blockquote>
+									Michael Hockey <span class="yellow">|</span> Director
+								</li>-->
+							</ul>
+						</div>
+					</div>
+
+
+					<div class="col-md-8">
+
+						<div class="row">
+
+							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
+								<h5 class="m-t-0">History</h5>
+								<p>5mm is a London-based but globally minded high end residential interiors design consultancy</p>
+							</div>
+
+							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
+								<h5 class="m-t-0">My Brief</h5>
+								<p>Our brief was to re-design 5mm's website to help them show off key projects and their fabulous photography.</p>
+							</div>
+														
+							<div class="col-sm-12 col-md-12 col-lg-12 m-b-30">
+								<h5 class="m-t-0">What I did</h5>
+								<p>We were under quite a tight budget and deadline for this project, so we stepped up to the challenge and opted to build 5mm a website using Twitter Bootstrap, which enabled us to efficiently build and design an elegant website around the framework.</p>
+								<p>We opted to use full-screen, scalable imagery for the clients photography to really help show off their work in the browser. The website is a means of serving this content and we didn't want the website to draw attention away from our clients amazing portfolio.</p>
+								<p>The website scales gracefully across different devices ensuring the content looks beautiful wherever their users happen to be.</p>
+								<?php include('../assets/php/includes/portfolio-cta.html');?>
+								
+							</div>
+
+						</div>
+
+					</div>
+
+				</div>
+				<!-- /PORTFOLIO DESCRIPTION -->			
+			</div>
+
+		</section>
+		<!-- /PORTFOLIO DESCRIPTION -->
+
+		<!-- PORTFOLIO CONTENT -->
+		<section class="module p-t-0 p-b-0">
+
+			<div class="container">
+
+				<div class="row">
+
+					<!--<div class="col-sm-12">-->
+						<div class="col-sm-8 col-sm-offset-4">
+
+						<p><img src="../assets/images/projects/5mm_portfolio_01.jpg" alt="5mm Mobile Website"></p>
+						<p><img src="../assets/images/projects/5mm_portfolio_05.jpg" alt="5mm Desktop Website"></p>
+						<p><img src="../assets/images/projects/5mm_portfolio_02.jpg" alt="5mm Portfolio Website"></p>
+						</div>
+
+				</div><!-- .row -->
+
+			</div>
+
+		</section>
+		<!-- /PORTFOLIO CONTENT -->
+
+		<?php include('../assets/php/includes/portfolio-pagination.html');?>
+		
+
+		<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+
+		<?php include('../assets/php/includes/footer-services.html');?>
+		<?php include('../assets/php/includes/footer-contact.html');?>
+		<?php include('../assets/php/includes/foot-scripts-internal.html');?>
+
+</body>
+</html>
