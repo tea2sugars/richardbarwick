0005f1a4141772a32182194075dbe0d458335b28
Rik Barwick <rik@creativitea.co.uk>
2019-04-15 17:24:13 +0100

    shopify page

---
+++ shopify-theme-development.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Rik Barwick: A Freelance Shopify Developer based in Suffolk, UK">
+    <meta name="author" content="Rik Barwick">
+
+    <title>Rik Barwick -Freelance Shopify Theme Developmer</title>
+
+    <?php include( 'assets/php/includes/head-css.html');?>
+
+
+    <!--Share widget-->
+    <script type="text/javascript">
+        var switchTo5x = true;
+    </script>
+    <script type="text/javascript" src="https://w.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript">
+        stLight.options({
+            publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f",
+            doNotHash: false,
+            doNotCopy: false,
+            hashAddressBar: false
+        });
+    </script>
+
+</head>
+
+<body>
+
+    <?php include( 'assets/php/includes/nav-transparent.html');?>
+
+
+    <!-- WRAPPER -->
+    <div class="wrapper">
+
+        <!-- HOME -->
+        <section class="module module-header bg-dark bg-dark-50" data-background="assets/images/e-commerce.jpg">
+
+            <div class="container">
+
+                <!-- MODULE TITLE -->
+                <div class="row">
+
+                    <div class="col-sm-6 col-sm-offset-3">
+
+                        <h1 class="module-title align-center">Looking for a Freelance Shopify Theme Developer?</h1>
+
+                        <div class="module-subtitle align-center">
+                            <p class="lead">You've come to the right place. Find out more about Shopify and how I can help your new online business.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /MODULE TITLE -->
+
+            </div>
+
+        </section>
+        <!-- /HOME -->
+
+        <section class="module-small">
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-8">
+
+
+                        <article>
+
+                            <h2>What is Shopify?</h2>
+                            <p><a href="https;//www.shopify.com" target="_blank">Shopify</a> is a hosted e-commerce platform for businesses catering for small cottage industries to large online stores. It is a subscription based service paying as little as £25 a month plus a small transaction fee. At the time of writing there are over 500,000 Shopify stores doing business contributing to over $40 billion worth of sales worldwide.</p>
+							<h3>Why use Shopify as a hosted solution?</h3>
+                            <p>Using a hosted solution basically means that Shopify owns the back-end code - The nuts and bolts of what makes their stores work and trade securely. This includes;
+
+<ul>
+	<li>Secure hosted environment with Shopify’s <a href="https://www.shopify.co.uk/pci-compliant" target="_blank">Certified Level 1 PCI DSS compliance</a></li>
+	<li>Automatic Updates</li>
+	<li>Responsive Framework so your store work on mobile</li>
+	<li>Payment Gateway so no delays from your bank</li>
+	<li>Extendable functionality thanks to their free and premium plugins</li>
+	<li>Simple usability for your store managers</li>
+</ul>
+
+This in essence saves your businesses money and time as a basic store could be trading in a matter of days - not months.</p>
+						<h3>Ease of use</h3>
+						<p>Shopify provide an excellent host of <a href="https://help.shopify.com/" target="_blank">training resources</a> and videos to help you manage your stores. As part of my service I will introduce you to the platform and show you how to add content to your store and manage your sales.</p>
+						<h3></h3>
+						<p>To get the most out of Shopify you will need to ‘theme’ your store. This is the part of the process that sets it apart from your competitors, utilising your brand and product assets to have a welcoming online store.
+						<h3>Shopify development</h3>
+						<p>As a <a href="https://www.shopify.co.uk/partners" target="_blank">Shopify Partner</a> I like to work on bespoke themes when budget permits. I do understand that budget is not always plentiful. There are a few base-themes that are easy to install and customise to suit lower budgets but as with any website I like to follow my usual design process;</p>
+						
+						<ul>
+							<li>Research & agree requirements</li>
+							<li>Design the UX & and wireframes and user journeys</li>
+							<li>Design the UI</li>
+							<li>Develop the site</li>
+							<li>Test the site on multiple devices and browsers</li>
+							<li>Launch the website so you can trade online</li>
+							<li>Research and test for improvements using live user data</li>
+						</ul>
+						<h3>How much does it cost?</h3>
+						<p>This is a loaded question and one I can’t really answer without knowing a bit more about the spec. I have built Shopify store themes from as little as £900 to £10,000. It really does depend on the level of customisation, functionality and scale. If you’d like to discuss your project and get a detailed quotation please get in touch.</p>
+                        </article>
+                    </div>
+                    
+                    <aside class="col-md-4">
+	                    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+							<script>
+							  hbspt.forms.create({
+								portalId: "5411762",
+								formId: "59b4d18a-101c-4e6e-8859-3e922749fe7e"
+							});
+							</script>
+                    </aside>
+                </div>
+            </div>
+        </section>
+        <section class="module-small light-grey-section" id="shopify-design-services">
+	        <div class="container">
+                <div class="row">
+                    <div class="col-md-2" class="text-center">
+	                    <img src="assets/images/rik-profile-image.png" alt="rik-profile-image" width="130" height="130" alt="Rik Barwick Profile" style="width:130px; margin: 0 auto;" >
+	                </div>
+	                <div class="col-md-6">
+		                <h4>My E-Commerce Design Experience</h4>
+		                <p>I have worked in e-commerce since before I completed my degree in 2004 having picked up various clients whilst studying.</p>
+		                <p>During my career have been fortunate enough to work in e-commerce for some of the world’s leading brands having designed experiences for GSK, Electronic Arts, Carphone Warehouse, TalkTalk, HSBC & Habitat.</p>
+		                <p>More recently I have been working start-ups and SMEs to help get them online selling including the build of my own wholesale focused e-commerce website for Hartest Brewing Co.</p>
+                        <div> <a href="contact" class="btn btn-lg btn-y btn-round">Book me today!</a></div>
+	                </div>
+	                
+	                <div class="col-md-4" style="margin-top: 30px;">
+		                <img src="assets/images/shopify-image.png" alt="shopify-image" width="360" height="287" class="width:100%; height:auto; ">
+	                </div>
+	                
+                </div>
+	        </div>
+        </section>
+       
+
+    <!-- DIVIDER -->
+    <hr class="divider-w">
+    <!-- /DIVIDER -->
+
+    <!-- PORTFOLIO -->
+    <section class="module-small p-t-20 p-b-40 p-t-sm-0" id="portfolio">
+        <div class="container-fluid">
+            <div class="row">
+                <h2 class="align-center">Some of my Website Design Projects</h2>
+            </div>
+            <div class="row" style="margin-top:20px;">
+                <!-- PORTFOLIO LIST -->
+                <ul id="works-grid" class="works-grid works-grid-4 works-hover-w">
+
+                    <!-- PORTFOLIO ITEM -->
+				<li class="work-item digital ux">
+					<a href="./5mm">
+						<div class="work-image">
+							<img src="assets/images/projects/5mm_portfolio_thumbnail.jpg" alt="5mm Interior Design Website Design"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">5mm.co</h3>
+							<div class="work-descr ">
+								Responsive Website Design
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+				
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item digital ux">
+					<a href="./buyapowa_saas">
+						<div class="work-image">
+							<img src="assets/images/projects/Buyapowa_SAAS_portfolio_thumbnail.jpg" alt="Buyapowa Social Commerce SAAS UX and Design">
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Buyapowa Social Commerce SAAS Platform</h3>
+							<div class="work-descr ">
+								UX & Website Design
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item digital branding ux">
+					<a href="./maxishop">
+						<div class="work-image">
+							<img src="assets/images/projects/maxishop_portfolio_thumbnail.jpg" alt="Maxishop Branding and E-Commerce">
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Maxishop.com</h3>
+							<div class="work-descr ">
+								E-Commerce UX & Information Architecture
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item digital">
+					<a href="./hallstreet">
+						<div class="work-image">
+							<img src="assets/images/projects/hallst_portfolio_thumbnail.png" alt="Hall Street"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Hall Street</h3>
+							<div class="work-descr ">
+								Responsive Website Design
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+                </ul>
+                <!-- /PORTFOLIO LIST -->
+            </div>
+        </div>
+
+    </section>
+    <!-- /PORTFOLIO -->
+
+    <!-- DIVIDER -->
+    <hr class="divider-w">
+    <!-- /DIVIDER -->
+
+    <?php include( 'assets/php/includes/footer-services.html');?>
+    <?php include( 'assets/php/includes/footer-contact.html');?>
+
+    <!-- Javascript files -->
+    <script src="assets/js/jquery-2.1.3.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <script src="assets/js/jquery.simple-text-rotator.min.js"></script>
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="assets/js/gmaps.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.js"></script>
+    <script src="assets/js/jquery.flexslider-min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.fitvids.js"></script>
+    <script src="assets/js/smoothscroll.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/contact.js"></script>
+    <script src="assets/js/custom.js"></script>
+
+</body>
+
+</html>
