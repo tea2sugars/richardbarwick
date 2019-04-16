261fab7d0a381d00a0c688e12a2acd307757823f
Rik Barwick <rik@creativitea.co.uk>
2019-04-16 09:15:30 +0100

    Updated template

--- website-design.php
+++ website-design.php
@@ -1,74 +1,85 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="A London, Surrey & Suffolk based Digital Design Agency working on Website Design, Social Media, Digital Marketing and SEO">
-	<meta name="author" content="">
-	
-	<title>Freelance Digital Designer, Suffolk & London| Rik Barwick</title>
-	
-		<?php include('assets/php/includes/head-css.html');?>
 
-	
-	<!--Share widget-->
-	<script type="text/javascript">var switchTo5x=true;</script>
-	<script type="text/javascript" src="https://w.sharethis.com/button/buttons.js"></script>
-	<script type="text/javascript">stLight.options({publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
-	
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Rik Barwick: A Freelance Website Designer & Developer based in Suffolk, UK">
+    <meta name="author" content="Rik Barwick">
+
+    <title>Rik Barwick -Freelance Website Designer & Developer</title>
+
+    <?php include( 'assets/php/includes/head-css.html');?>
+
+
+    <!--Share widget-->
+    <script type="text/javascript">
+        var switchTo5x = true;
+    </script>
+    <script type="text/javascript" src="https://w.sharethis.com/button/buttons.js"></script>
+    <script type="text/javascript">
+        stLight.options({
+            publisher: "0750579a-dfe6-4d2b-a96d-da69d0aa188f",
+            doNotHash: false,
+            doNotCopy: false,
+            hashAddressBar: false
+        });
+    </script>
+
 </head>
+
 <body>
 
-	<?php include('assets/php/includes/nav-transparent.html');?>
+    <?php include( 'assets/php/includes/nav-transparent.html');?>
 
 
-	<!-- WRAPPER -->
-	<div class="wrapper">
+    <!-- WRAPPER -->
+    <div class="wrapper">
 
-		<!-- HOME -->
-		<section class="module module-header bg-dark bg-dark-50" data-background="assets/images/digital_header.jpg">
+        <!-- HOME -->
+        <section class="module module-header bg-dark bg-dark-50" data-background="assets/images/e-commerce.jpg">
 
-			<div class="container">
+            <div class="container">
 
-				<!-- MODULE TITLE -->
-				<div class="row">
+                <!-- MODULE TITLE -->
+                <div class="row">
 
-					<div class="col-sm-6 col-sm-offset-3">
+                    <div class="col-sm-6 col-sm-offset-3">
 
-						<h1 class="module-title align-center">Freelance Website Design</h1>
+                        <h1 class="module-title align-center">Freelance Website Design and Development</h1>
 
-						<div class="module-subtitle align-center">
-							<p class="lead">Helping deliver your brand message and your product marketing instantly anywhere in the world.</p>
-							<a class="btn btn-lg btn-y btn-round" href="#digital-services"><i class="fa fa-arrow-down"></i> Learn more</a>
+                        <div class="module-subtitle align-center">
+                            <p class="lead">Helping deliver your brand message and your product marketing instantly anywhere in the world.</p>
+                        </div>
+                    </div>
 
-						</div>
-					</div>
+                </div>
+                <!-- /MODULE TITLE -->
 
-				</div>
-				<!-- /MODULE TITLE -->
+            </div>
 
-			</div>
+        </section>
+        <!-- /HOME -->
 
-		</section >
-		<!-- /HOME -->
+        <section class="module-small">
 
-		<section class="module" id="digital-services">
+            <div class="container">
 
-			<div class="container">
-				
-				<div class="row">
-					<div class="col-sm-8 col-sm-offset-2">
-						
+                <div class="row">
+                    <div class="col-md-8">
 
-							<article>
 
-								<h3>I'm a digital professional that prides himself in his creative thinking, technical knowhow and a thirst for customer satisfaction.</h3>
-								<p>Over the years I've worked independently or with experienced digital designers, strategists and developers that can adapt existing services, innovate and develop digital projects that cater for any budget and timeframe. With creative digital thinking there's always a way!</p>
+                        <article>
+
+                            <h2>Website design that works</h2>
+                            <h3>I'm a digital professional that prides himself in his creative thinking, technical knowhow and a thirst for customer satisfaction.</h3>
+                            <p>Over the years I've worked independently or with experienced digital designers, strategists and developers that can adapt existing services, innovate and develop digital projects that cater for any budget and timeframe. With creative digital thinking there's always a way!</p>
 								
 								<p>It's never been more important for your brand to engage with your audience via a range of digital channels. I collaborate with my clients to deliver digital design solutions that deliver great results. I take time to understand your business aspirations and the culture surrounding your brand in order to partner with you at any point in your digital journey. From a designing a website to developing campaigns on social media, mobile and digital marketing.</p>
 								
-								<h4>Digital Design Solutions</h4>
-								<p>I pride myself on being able to design clients beautiful design solutions with technical knowhow to backup our thought and strategy. Discover more about our digital services below;</p>
+						<h3>Creative digital design solutions</h3>
+						
+						<p>I pride myself on being able to design clients beautiful design solutions with technical knowhow to backup our thought and strategy. Discover more about our digital services below;</p>
 								<p>
 									<ul>
 										
@@ -82,28 +93,131 @@
 										</li>
 									</ul>
 								</p>
+						<p>I help clients plan, implement and evolve their websites and other digital products so they deliver results. If you have a project you'd like to have a chat about please <a href="contact">get in touch</a></p>
+                        </article>
+                    </div>
+                    
+                    <aside class="col-md-4">
+	                    <h2>Get in touch</h2>
+	                    <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/shell.js"></script>
+							<script>
+							  hbspt.forms.create({
+								portalId: "5411762",
+								formId: "59b4d18a-101c-4e6e-8859-3e922749fe7e"
+							});
+							</script>
+                    </aside>
+                </div>
+            </div>
+        </section>
+        <section class="module-small light-grey-section" id="shopify-design-services">
+	        <div class="container">
+                <div class="row">
+                    <div class="col-md-2" class="text-center">
+	                    <img src="assets/images/rik-profile-image.png" alt="rik-profile-image" width="130" height="130" alt="Rik Barwick Profile" style="width:130px; margin: 0 auto;" >
+	                </div>
+	                <div class="col-md-6">
+		                <h4>My E-Commerce Design Experience</h4>
+		                <p>I have worked in e-commerce since before I completed my degree in 2004 having picked up various clients whilst studying.</p>
+		                <p>During my career have been fortunate enough to work in e-commerce for some of the world’s leading brands having designed experiences for GSK, Electronic Arts, Carphone Warehouse, TalkTalk, HSBC & Habitat.</p>
+		                <p>More recently I have been working start-ups and SMEs to help get them online selling including the build of my own wholesale focused e-commerce website for Hartest Brewing Co.</p>
+                        <div> <a href="contact" class="btn btn-lg btn-y btn-round">Book me today!</a></div>
+	                </div>
+	                
+	                <div class="col-md-4" style="margin-top: 30px;">
+		                <img src="assets/images/shopify-image.png" alt="shopify-image" width="360" height="287" class="width:100%; height:auto; ">
+	                </div>
+	                
+                </div>
+	        </div>
+        </section>
+       
 
-								
-							</article>
-						
-					</div>
-				</div>
-				<div class="row">
-					<div class="col-sm-8 col-sm-offset-2">
-						<p>I help clients plan, implement and evolve their websites and other digital products so they deliver results. If you have a project you'd like to have a chat about please contact me.</p>
-							<a href="contact" class="btn btn-d btn-round">Contact Us</a> <a href="portfolio" class="btn btn-g btn-round">View work</a>
-					</div>
-							
-						<!-- /POST -->
+    <!-- DIVIDER -->
+    <hr class="divider-w">
+    <!-- /DIVIDER -->
 
-					</div>
-				</div>
-			</div>
-		</section>
-		
-				<!-- DIVIDER -->
-		<hr class="divider-w">
-		<!-- /DIVIDER -->
+    <!-- PORTFOLIO -->
+    <section class="module-small p-t-20 p-b-40 p-t-sm-0" id="portfolio">
+        <div class="container-fluid">
+            <div class="row">
+                <h2 class="align-center">Some of my Website Design Projects</h2>
+            </div>
+            <div class="row" style="margin-top:20px;">
+                <!-- PORTFOLIO LIST -->
+                <ul id="works-grid" class="works-grid works-grid-4 works-hover-w">
+
+                    <!-- PORTFOLIO ITEM -->
+				<li class="work-item digital ux">
+					<a href="./5mm">
+						<div class="work-image">
+							<img src="assets/images/projects/5mm_portfolio_thumbnail.jpg" alt="5mm Interior Design Website Design"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">5mm.co</h3>
+							<div class="work-descr ">
+								Responsive Website Design
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+				
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item digital ux">
+					<a href="./buyapowa_saas">
+						<div class="work-image">
+							<img src="assets/images/projects/Buyapowa_SAAS_portfolio_thumbnail.jpg" alt="Buyapowa Social Commerce SAAS UX and Design">
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Buyapowa Social Commerce SAAS Platform</h3>
+							<div class="work-descr ">
+								UX & Website Design
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item digital branding ux">
+					<a href="./maxishop">
+						<div class="work-image">
+							<img src="assets/images/projects/maxishop_portfolio_thumbnail.jpg" alt="Maxishop Branding and E-Commerce">
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Maxishop.com</h3>
+							<div class="work-descr ">
+								E-Commerce UX & Information Architecture
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+				<!-- PORTFOLIO ITEM -->
+				<li class="work-item digital">
+					<a href="./hallstreet">
+						<div class="work-image">
+							<img src="assets/images/projects/hallst_portfolio_thumbnail.png" alt="Hall Street"/>
+						</div>
+						<div class="work-caption">
+							<h3 class="work-title ">Hall Street</h3>
+							<div class="work-descr ">
+								Responsive Website Design
+							</div>
+						</div>
+					</a>
+				</li>
+				<!-- /PORTFOLIO ITEM -->
+                </ul>
+                <!-- /PORTFOLIO LIST -->
+            </div>
+        </div>
+
+    </section>
+    <!-- /PORTFOLIO -->
+
+    <!-- DIVIDER -->
+    <hr class="divider-w">
+    <!-- /DIVIDER -->
 		
 		<section class="module-small light-grey-section" id="web-services">
 	        <div class="container-fluid">
@@ -161,117 +275,29 @@
                 </div>
 	        </div>
         </section>
-        
-        <!-- DIVIDER -->
-		<hr class="divider-w">
-		<!-- /DIVIDER -->
-		
-		<!-- PORTFOLIO -->
-		<section class="module-small p-t-20 p-b-40 p-t-sm-0" id="portfolio">
-			<div class="container-fluid">
-				<div class="row">
-					<h2 class="align-center">Some of my Website Design Projects</h2>
-				</div>
-				<div class="row">
-					<!-- PORTFOLIO LIST -->
-			<ul id="works-grid" class="works-grid works-grid-4 works-hover-w">
-				
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item branding print branding digital">
-					<a href="./portfolio/domenico">
-						<div class="work-image">
-							<img src="assets/images/projects/Domenico_portfolio_thumbnail.jpg" alt="Domenico Pizza Branding"/>
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Domenico Pizza</h3>
-							<div class="work-descr ">
-								Hand-Crafted Branding &amp; Digital Design
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
 
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item digital">
-					<a href="./portfolio/buyapowa_saas">
-						<div class="work-image">
-							<img src="assets/images/projects/Buyapowa_SAAS_portfolio_thumbnail.jpg" alt="Buyapowa Social Commerce SAAS UX and Design">
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Buyapowa Social Commerce SAAS Platform</h3>
-							<div class="work-descr ">
-								UX & Digital Design
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item digital">
-					<a href="./portfolio/5mm">
-						<div class="work-image">
-							<img src="assets/images/projects/5mm_portfolio_thumbnail.jpg" alt="5mm Interior Design Website Design"/>
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">5mm.co</h3>
-							<div class="work-descr ">
-								Website Design
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-				<!-- PORTFOLIO ITEM -->
-				<li class="work-item digital">
-					<a href="./portfolio/djst">
-						<div class="work-image">
-							<img src="assets/images/projects/djst_portfolio_thumbnail.jpg" alt="Don't Just Sit There, Sell Something Retro website Design"/>
-						</div>
-						<div class="work-caption">
-							<h3 class="work-title ">Don't Just Sit There, Sell Something</h3>
-							<div class="work-descr ">
-								Small Business Website Design
-							</div>
-						</div>
-					</a>
-				</li>
-				<!-- /PORTFOLIO ITEM -->
-				
-				
-							</ul>
-			<!-- /PORTFOLIO LIST -->
-				</div>
-			</div>
-			
-		</section>
-		<!-- /PORTFOLIO -->
-		
-		<!-- DIVIDER -->
-		<hr class="divider-w">
-		<!-- /DIVIDER -->
-		
-		<?php include('assets/php/includes/footer-services.html');?>
-		<?php include('assets/php/includes/footer-contact.html');?>
-	
-	<!-- Javascript files -->
-	<script src="assets/js/jquery-2.1.3.min.js"></script>
-	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
-	<script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
-	<script src="assets/js/appear.js"></script>
-	<script src="assets/js/jquery.simple-text-rotator.min.js"></script>
-	<script src="assets/js/jqBootstrapValidation.js"></script>
-	<script src="https://maps.google.com/maps/api/js?sensor=true"></script>
-	<script src="assets/js/gmaps.js"></script>
-	<script src="assets/js/isotope.pkgd.min.js"></script>
-	<script src="assets/js/imagesloaded.pkgd.js"></script>
-	<script src="assets/js/jquery.flexslider-min.js"></script>
-	<script src="assets/js/jquery.magnific-popup.min.js"></script>
-	<script src="assets/js/jquery.fitvids.js"></script>
-	<script src="assets/js/smoothscroll.js"></script>
-	<script src="assets/js/wow.min.js"></script>
-	<script src="assets/js/contact.js"></script>
-	<script src="assets/js/custom.js"></script>
+    <?php include( 'assets/php/includes/footer-services.html');?>
+    <?php include( 'assets/php/includes/footer-contact.html');?>
+
+    <!-- Javascript files -->
+    <script src="assets/js/jquery-2.1.3.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/jquery.mb.YTPlayer.min.js"></script>
+    <script src="assets/js/appear.js"></script>
+    <script src="assets/js/jquery.simple-text-rotator.min.js"></script>
+    <script src="assets/js/jqBootstrapValidation.js"></script>
+    <script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="assets/js/gmaps.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.js"></script>
+    <script src="assets/js/jquery.flexslider-min.js"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="assets/js/jquery.fitvids.js"></script>
+    <script src="assets/js/smoothscroll.js"></script>
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/contact.js"></script>
+    <script src="assets/js/custom.js"></script>
 
 </body>
+
 </html>
