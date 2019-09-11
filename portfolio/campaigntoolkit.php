6680f892fc163136dc7535ae669e7c05300f7c7b
Rik Barwick <rik@creativitea.co.uk>
2019-09-11 16:55:18 +0100

    ISD portfolio page

---
+++ portfolio/campaigntoolkit.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Rik Barwick worked with Fat Beehive to redesign and develop the front-end design for the new ISD Campaign Toolkit.">
+	<meta name="author" content="Rik Barwick">
+	
+	<title>ISD Campaign Toolkit Website Redesign | Front-end development by Rik Barwick and Fat Beehive</title>
+	
+	<?php include('../assets/php/includes/head-css-internal.html');?>
+</head>
+<body>
+
+	<?php include('../assets/php/includes/nav-internal.html');?>
+
+
+	<!-- HOME -->
+	<section class="module module-header bg-dark bg-dark-50" data-background="../assets/images/projects/ISD/isd-page-header.jpg">
+
+			<div class="container">
+
+				<!-- MODULE TITLE -->
+				<div class="row">
+
+					<div class="col-sm-6 col-sm-offset-3">
+
+						<h1 class="module-title align-center">ISD Campaign Toolkit Website Design<br/><span class="small">Front-end development.</span></h1>
+
+
+					</div>
+
+				</div>
+				<!-- /MODULE TITLE -->
+
+			</div>
+
+		</section >
+	<!-- /HOME -->
+
+	<!-- WRAPPER -->
+	<div class="wrapper">
+
+		<!-- PORTFOLIO DESCRIPTION -->
+		<section class="module-small">
+
+			<div class="container">
+				
+				<!--<div class="row">
+					<div class="col-sm-12">
+
+						<p><img src="../assets/images/section-1.jpg" alt=""></p>
+					</div>
+				</div>-->
+				
+				
+
+				<!-- PORTFOLIO DESCRIPTION -->
+				<div class="row m-t-70">
+
+					<div class="col-md-4">
+						<div class="work-details">
+							<ul>
+								<li><strong>Client: </strong>ISD</li>
+								<li><strong>Agency: </strong><a href="https://www.fatbeehive.com" target="_blank">Fat Beehive</a></li>
+								<li><strong>Project: </strong>Website Design</li>
+								<li><strong>Online: </strong><a href="https://www.campaigntoolkit.org" target="_blank">campaigntoolkit.org</a></li>
+								<li><strong>Budget: </strong>££££</li>
+								<!--<li><strong>Testimonial: </strong>
+									<br/>
+									<blockquote>Being a new company, FiveThree had lots of things to worry about - the creation and development of a brand identity wasn't one of them thanks to the creative thinking at Creativitea.</blockquote>
+									Michael Hockey <span class="yellow">|</span> Director
+								</li>-->
+							</ul>
+						</div>
+					</div>
+
+
+					<div class="col-md-8">
+
+						<div class="row">
+
+							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
+								<h5 class="m-t-0">History</h5>
+								<p>The Institute for Strategic Dialogue (ISD) are a client of Fat Beehive. The Camapign Toolkit is an interactive educational resource to support activists and organisations with creating effective and impactful online campaigns that promote community cohesion, inclusion and tolerance.</p>
+							</div>
+
+							<div class="col-sm-6 col-md-6 col-lg-6 m-b-30">
+								<h5 class="m-t-0">My Brief</h5>
+								<p>My brief from Fat Beehive was to build and design the front-end of the website for the agencies back-end developers to then install their chosen CMS. The design is based on the new branding designed by the client and Fat Beehive.</p>
+							</div>
+											
+							
+						</div>
+
+					</div>
+
+				</div>
+				<!-- /PORTFOLIO DESCRIPTION -->			
+			</div>
+
+		</section>
+		<!-- /PORTFOLIO DESCRIPTION -->
+		
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-11 col-sm-offset-1">
+						<img src="../assets/images/projects/ISD/isd-portfolio-header.jpg" alt="ISD's Campaign Toolkit">
+					</div>
+					<div class="col-md-8 col-sm-offset-1">
+						<h3 class="m-t-0">What I did</h3>
+						<p>Fat Beehive was tasked with building the new Campaign Toolkit website designed to help activists create their campaigns to help raise awareness. I worked with the agency as part of a remote team to put together the new front-end code (HTML and CSS) for the new design which was later implemented into the Drupal CMS.</p>
+						<p>Throughout this project I was tasked with making creative decisions to implement new branding and illustration elements. The homepage is quite complex with it's overlapping illustrations and curve patterns which caused a few challenges when dealing with responsive layouts - especially with smaller mobile devices. I broke these up into several layers and positioned them as separate background images along the site's breakpoints.</p>
+						<p>The resulting homepage is soft and approachable for the users. The toolkit is easy to use and offers the user all the information they need to create successful campaigns.</p>
+						<p>It's work like this that has the potential to change the world for the better. One I am very proud of playing my part.</p>
+					</div>
+					<div class="col-md-11 col-sm-offset-1">
+						<p><img src="../assets/images/projects/ISD/isd-portfolio-01.jpg" alt="ISD's Campaign Toolkit"></p>
+						<p><img src="../assets/images/projects/ISD/isd-portfolio-02.jpg" alt="ISD's Campaign Toolkit"></p>
+					</div>
+				</div>
+		</section>
+
+		
+
+		<?php include('../assets/php/includes/portfolio-pagination.html');?>
+		
+
+		<!-- DIVIDER -->
+		<hr class="divider-w">
+		<!-- /DIVIDER -->
+
+		<?php include('../assets/php/includes/footer-services.html');?>
+		<?php include('../assets/php/includes/footer-contact.html');?>
+		<?php include('../assets/php/includes/foot-scripts-internal.html');?>
+
+</body>
+</html>
